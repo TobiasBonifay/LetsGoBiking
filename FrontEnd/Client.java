b3d20264cb8e17775f213c7157009e72eebc6fb6
TobiasBonifay <tobias.bonifay@etu.univ-cotedazur.fr>
2022-11-16 15:56:28 +0100

    init

---
+++ FrontEnd/Client.java
@@ -0,0 +1,15 @@
+/**
+ * Java client (SOAP)
+ */
+public class Client {
+    public static void main(String[] args) {
+        // Create a new instance of the client
+        Client client = new Client();
+        // Start the client
+        client.start();
+    }
+
+    // Start the client
+    public void start() {
+    }
+}
