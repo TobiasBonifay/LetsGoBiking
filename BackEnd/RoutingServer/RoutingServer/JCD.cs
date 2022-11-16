528d8ee4d10102d812b489c6ceea4e6f232a2504
unknown <ihor.melnyk@etu.unice.fr>
2022-11-16 17:44:13 +0100

    Début de JCDClient

--- BackEnd/RoutingServer/RoutingServer/JCD.cs
+++ BackEnd/RoutingServer/RoutingServer/JCD.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,12 @@ namespace RoutingServer
 {
     public class JCD
     {
-        
+        private HttpClient client;
+        private static string apiKey = "";
+
+        public void InitializeConnexion()
+        {
+            client = new HttpClient();
+        }
     }
 }
