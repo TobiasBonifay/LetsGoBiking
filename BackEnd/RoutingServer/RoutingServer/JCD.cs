6de2ca858c43d1b25db175dac01c2381dd37816d
unknown <ihor.melnyk@etu.unice.fr>
2022-11-16 16:54:44 +0100

    Création du Routing Server

--- BackEnd/RoutingServer/RoutingServer/JCD.cs
+++ BackEnd/RoutingServer/RoutingServer/JCD.cs
@@ -6,10 +6,7 @@ using System.Threading.Tasks;
 
 namespace RoutingServer
 {
-    internal class Program
+    internal class Class1
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
