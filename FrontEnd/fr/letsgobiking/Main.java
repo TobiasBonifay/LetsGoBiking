045882271bfcb6bf35cc61e140d896e7bd9fd491
TobiasBonifay <tobias.bonifay@etu.univ-cotedazur.fr>
2022-11-16 17:37:43 +0100

    Client in progress

--- FrontEnd/fr/letsgobiking/Main.java
+++ FrontEnd/fr/letsgobiking/Main.java
@@ -1,7 +1,9 @@
 package fr.letsgobiking;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // Create a new instance of the client
         Client client = new Client();
         // Start the client
--- FrontEnd/fr/letsgobiking/soap/ISoap.java
+++ FrontEnd/fr/letsgobiking/soap/ISoap.java
@@ -1,6 +1,12 @@
 package fr.letsgobiking.soap;
 
+import java.io.IOException;
+
 public interface ISoap {
     String sendSoapRequest(String start, String end);
+
+    void openConnection() throws IOException;
+
+    void closeConnection();
 
 }
