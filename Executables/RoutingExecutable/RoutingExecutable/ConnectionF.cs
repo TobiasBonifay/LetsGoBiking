fd426c430750e7a7e2c7dab449a70298b17fd5bc
TobiasBonifay <tobias.bonifay@etu.univ-cotedazur.fr>
2022-12-07 10:53:40 +0100

    HOT FIX

---
+++ Executables/RoutingExecutable/RoutingExecutable/ConnectionF.cs
@@ -0,0 +1,22 @@
+﻿using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+
+
+namespace RoutingExecutable.ORSClasses
+{
+    public class ConnectionF
+    {
+        public IConnection CreateConnection()
+        {
+            return CreateConnection("activemq:tcp://localhost:61616");
+        }
+
+        public IConnection CreateConnection(string uri)
+        {
+            var factory = new Apache.NMS.ActiveMQ.ConnectionFactory(uri);
+            IConnection connection = factory.CreateConnection("user", "admin");
+            connection.Start();
+            return connection;
+        }
+    }
+}
